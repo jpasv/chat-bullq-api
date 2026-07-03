04fb61f8e80141e24e8e64a085737ad85b1e652a
Kleber A dos Santos <kleber.dossantos001@mymdc.net>
2026-07-03 13:13:22 -0400

    feat(webhooks): add constants

---
+++ src/modules/webhooks/webhooks.constants.ts
@@ -0,0 +1,6 @@
+export const WEBHOOK_QUEUE = 'webhook-deliveries';
+export const MAX_WEBHOOK_ATTEMPTS = 5;
+export const WEBHOOK_BACKOFF_MS = 5_000;
+export const WEBHOOK_AUTO_DISABLE_AFTER = 10;
+export const WEBHOOK_TIMEOUT_MS = 10_000;
+export const WEBHOOK_SECRET_PREFIX = 'whsec_';
